eae0190d8c59f033ea2bbc21e3408697257ca068
chrisgoodell <cg53142@fastmail.com>
2018-04-28 19:38:20 -0400

    Add state to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './App.css'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2018'
+    }
+  }
+
   render() {
     return (
-    <div>
-      Countdown Champ, App Component
+    <div className="App">
+      <div className="title">Countdown to {this.state.deadline}</div>
+      <div>
+        <div className="timeItem">14 days</div>
+        <div className="timeItem">6 hours</div>
+        <div className="timeItem">27 minutes</div>
+        <div className="timeItem">42 seconds</div>
+      </div>
+      <div>
+        <input placeholder="new date" />
+        <button>Submit</button>
+      </div>
     </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-<div>
-  countdown champ
-</div>
-
 ReactDOM.render(<App />, document.getElementById('root'));

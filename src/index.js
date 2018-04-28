ed685822e0d8cd616a1e9c10a38b20aa22fe782b
chrisgoodell <cg53142@fastmail.com>
2018-04-28 19:14:46 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+<div>
+  countdown champ
+</div>
+
+ReactDOM.render(<div>
+  countdown champ
+</div>, document.getElementById('root'));
